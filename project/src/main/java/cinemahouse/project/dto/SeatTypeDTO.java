4f511b8ef36322c91d5c07e49769b14153374bcb
huypqse <huypqse@gmail.com>
2025-02-16 22:46:24 +0700

    add seatype dto

---
+++ project/src/main/java/cinemahouse/project/dto/SeatTypeDTO.java
@@ -0,0 +1,14 @@
+package cinemahouse.project.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SeatTypeDTO {
+    String name;
+}
