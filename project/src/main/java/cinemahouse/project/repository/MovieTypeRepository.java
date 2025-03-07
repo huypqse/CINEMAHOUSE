df3f43f5551245fdf4218029e0d003a104fa4a56
huypqse <huypqse@gmail.com>
2025-03-07 15:33:14 +0700

    feat: feat MovieTypeRepository.java

--- project/src/main/java/cinemahouse/project/repository/MovieTypeRepository.java
+++ project/src/main/java/cinemahouse/project/repository/MovieTypeRepository.java
@@ -4,6 +4,11 @@ import cinemahouse.project.entity.MovieType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 @Repository
 public interface MovieTypeRepository extends JpaRepository<MovieType, Long> {
+    Optional<Set<MovieType>> findAllByIdIn(List<Long> id);
 }
