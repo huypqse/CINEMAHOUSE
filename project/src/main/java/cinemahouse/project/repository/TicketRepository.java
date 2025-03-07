4ca1fe161a27760a6e689f6809635b4cfb0c23a8
huypqse <huypqse@gmail.com>
2025-03-07 15:17:45 +0700

    feat: feat TicketRepository.java

--- project/src/main/java/cinemahouse/project/repository/TicketRepository.java
+++ project/src/main/java/cinemahouse/project/repository/TicketRepository.java
@@ -4,7 +4,11 @@ import cinemahouse.project.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
-
+    List<Ticket> findAllByIdIn(List<Long> ids);
+    Set<Ticket> findByBill_Id(Long id);
 }
