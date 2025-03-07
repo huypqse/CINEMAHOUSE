7b1b2e94a2e834ee3914d786cc525dbcb7d89d17
huypqse <huypqse@gmail.com>
2025-03-07 13:38:07 +0700

    chore: fix bug CustomElasticsearchPersistentProperty.java class

--- project/src/main/java/cinemahouse/project/configuration/elasticsearch/CustomElasticsearchPersistentProperty.java
+++ project/src/main/java/cinemahouse/project/configuration/elasticsearch/CustomElasticsearchPersistentProperty.java
@@ -1,20 +1,20 @@
-package cinemahouse.project.configuration.elasticsearch;
-
-import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchPersistentProperty;
-import org.springframework.data.mapping.PersistentEntity;
-import org.springframework.data.mapping.model.Property;
-import org.springframework.data.mapping.model.SimpleTypeHolder;
-
-public class CustomElasticsearchPersistentProperty extends SimpleElasticsearchPersistentProperty {
-    @SuppressWarnings({ "unchecked" })
-    public CustomElasticsearchPersistentProperty(Property property, PersistentEntity owner, SimpleTypeHolder simpleTypeHolder) {
-        super(property, owner, simpleTypeHolder);
-    }
-
-    @Override
-    public boolean isAssociation() {
-        return false;
-    }
-
-}
-
+//package cinemahouse.project.configuration.elasticsearch;
+//
+//import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchPersistentProperty;
+//import org.springframework.data.mapping.PersistentEntity;
+//import org.springframework.data.mapping.model.Property;
+//import org.springframework.data.mapping.model.SimpleTypeHolder;
+//
+//public class CustomElasticsearchPersistentProperty extends SimpleElasticsearchPersistentProperty {
+//    @SuppressWarnings({ "unchecked" })
+//    public CustomElasticsearchPersistentProperty(Property property, PersistentEntity owner, SimpleTypeHolder simpleTypeHolder) {
+//        super(property, owner, simpleTypeHolder);
+//    }
+//
+//    @Override
+//    public boolean isAssociation() {
+//        return false;
+//    }
+//
+//}
+//
